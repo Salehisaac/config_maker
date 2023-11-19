ed7f1b86c25a2a878e25ba98e183ba6404cdaf10
saleh <99868995+Salehisaac@users.noreply.github.com>
2023-11-20 00:36:20 +0330

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,10 @@
 use app\TelegramBot;
 require_once 'TelegramBot.php';
 
-$url_panel = '';
-$proxies = array();
-$expire = '';
-$data_limit = '';
-$username = '';
 
 
 
 $token = "6813131583:AAHhfKYcObFrXsuzZ-7oZD_ldi6X2rU4K-k";
-$offset = 861718664;
-$url = "https://api.telegram.org/bot6813131583:AAHhfKYcObFrXsuzZ-7oZD_ldi6X2rU4K-k/getUpdates?offset{$offset}";
 $action = "sendMessage";
 
 $bot = new TelegramBot($token);
@@ -27,13 +20,6 @@ vardump($update);
 
 
 
-$result = file_get_contents($url);
-
-
-$arrResult = json_decode($result, true);
-$length = count($arrResult['result']);
-$chatId = $arrResult['result'][0]["message"]["chat"]["id"];
-$text = $arrResult['result'][$length - 1]['message']['text'] ;
 
 
 
@@ -43,27 +29,10 @@ $text = $arrResult['result'][$length - 1]['message']['text'] ;
 
 
 
-// $bot->handleInOrder($chatId , $text);
-
-// Process the received data
 
 
-function get_update_id($updates)
-    {
-        $num_updates = count($updates["result"]);
-        $last_update = $num_updates - 1 ;
-        $update_id = $updates["result"][$last_update]["update_id"];
-        return ($update_id);
-    }
 
 
-function get_updates($offset)
-{
-
-    $url =   "https://api.telegram.org/bot6809114912:AAEnGhJ_em9lf9I1uofJAXfkiiVd8AgFOyE/getUpdates?offset={$offset}";
-    $json = file_get_contents($url);
-    return json_decode($json, true);
-}
 
 
 
