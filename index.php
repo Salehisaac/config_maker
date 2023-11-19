78f558c92142850a37e94cef079a636d1e5857a6
saleh <99868995+Salehisaac@users.noreply.github.com>
2023-11-20 00:47:43 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,43 @@
 <?php
 
-
-
-
 use app\TelegramBot;
+
 require_once 'TelegramBot.php';
-
-
-
 
 $token = "6813131583:AAHhfKYcObFrXsuzZ-7oZD_ldi6X2rU4K-k";
 $action = "sendMessage";
 $text = 'salam';
 
-
-
-
-
 $bot = new TelegramBot($token);
 
 $data = file_get_contents("php://input");
 $update = json_decode($data, true);
-$chatId = $update ['message']['chat']['id'];
 
-   $data = [
-                'chat_id' => $chatId,
-                'text' => $text,
+// Check if the 'message' key is present in the update
+if (isset($update['message'])) {
+    $chatId = $update['message']['chat']['id'];
 
-            ];
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-            echo $response;
-            curl_close($ch);
+    $url = "https://api.telegram.org/bot$token/$action";
 
+    $data = [
+        'chat_id' => $chatId,
+        'text' => $text,
+    ];
 
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
 
+    // Check for cURL errors
+    if (curl_errno($ch)) {
+        echo 'cURL error: ' . curl_error($ch);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    echo $response;
+    curl_close($ch);
+} else {
+    echo 'No valid message in the update.';
+}
